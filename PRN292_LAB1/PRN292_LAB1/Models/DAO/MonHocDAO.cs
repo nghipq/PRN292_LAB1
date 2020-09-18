936799e23b6fac3e9ffc80c6c6b54aa89bc123b7
nghipq <nghipqce140179@fpt.edu.vn>
2020-09-18 12:50:28 +0700

    create models folder & DAO folder

---
+++ PRN292_LAB1/PRN292_LAB1/Models/DAO/MonHocDAO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRN292_LAB1.Models.DAO
+{
+    class MonHocDAO
+    {
+    }
+}
---
+++ PRN292_LAB1/PRN292_LAB1/Models/Entity/MonHoc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRN292_LAB1.Models.Entity
+{
+    class MonHoc
+    {
+    }
+}
