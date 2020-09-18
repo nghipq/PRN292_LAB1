07b9fafd343facb0ad0323dfaceb22ee305fb872
nghipq <nghipqce140179@fpt.edu.vn>
2020-09-18 12:46:15 +0700

    create new PJ

---
+++ PRN292_LAB1/PRN292_LAB1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRN292_LAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
