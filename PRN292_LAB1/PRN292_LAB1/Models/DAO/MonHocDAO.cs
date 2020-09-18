be6ea9a1c292d6419d93de983b25568088860842
nghipq <nghipqce140179@fpt.edu.vn>
2020-09-18 13:40:22 +0700

    create MonHoc models

--- PRN292_LAB1/PRN292_LAB1/Models/DAO/MonHocDAO.cs
+++ PRN292_LAB1/PRN292_LAB1/Models/DAO/MonHocDAO.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using PRN292_LAB1.Models.Entity;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace PRN292_LAB1.Models.DAO
 {
     class MonHocDAO
     {
-        public MonHocDAO() { }
+        List<MonHoc> listMH = new List<MonHoc>();   //danh sach mon hoc
+        public MonHocDAO()
+        {
+            String filename = "../../../Models/Data/MonHoc.txt";
+            FileStream fs;
+
+            if (File.Exists(filename))
+            {
+                fs = new FileStream(filename, FileMode.Open);
+            }
+            else
+            {
+                fs = File.Create(filename);
+            }
+
+            StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+            Console.WriteLine(rd.ReadToEnd());
+
+            rd.Close();
+        }
     }
 }
