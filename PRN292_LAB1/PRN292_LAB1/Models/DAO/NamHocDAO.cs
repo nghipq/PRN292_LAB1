5782c82e8cfd8b7b840c02637a87223300792945
NganHua <NganHTCE140552@fpt.edu.vn>
2020-09-18 13:41:42 +0700

    NamHoc

---
+++ PRN292_LAB1/PRN292_LAB1/Models/DAO/NamHocDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRN292_LAB1.Models.DAO
+{
+    class NamHocDAO
+    {
+        public NamHocDAO()
+        {
+            
+        }
+    }
+}
